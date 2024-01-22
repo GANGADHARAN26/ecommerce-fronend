0a66dcb3ec04f784dfff36e0f83ae596e5be0b79
GANGADHARAN26 <gangadharana01@gmail.com>
2024-01-22 21:51:29 +0530

    added contact information

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -240,7 +240,7 @@ const Checkout = () => {
                 </ol>
               </nav>
               <h4 className="title total">Contact Information</h4>
-              <p className="user-details total">Myname (myemail@gmail.com)</p>
+              <p className="user-details total">Gangadharan (gangadharana01@gmail.com)</p>
               <h4 className="mb-3">Shipping Address</h4>
               <form
                 onSubmit={formik.handleSubmit}
