594cd13a9d63e6878988840a9b770a6ffcbda956
GANGADHARAN26 <gangadharana01@gmail.com>
2024-01-23 21:32:23 +0530

    changed checkout payment error -1

--- src/utils/axioConfig.js
+++ src/utils/axioConfig.js
@@ -1,5 +1,5 @@
-// export const base_url="https://ecommerce-backend-yp85.onrender.com/api/";
-export const base_url="http://localhost:5050/api/";
+export const base_url="https://ecommerce-backend-yp85.onrender.com/api/";
+// export const base_url="http://localhost:5050/api/";
 
 const getTokenFromLocalStorage = localStorage.getItem("customer")
   ? JSON.parse(localStorage.getItem("customer"))
